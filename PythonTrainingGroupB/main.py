fc2eee4f88e5cba2a0ddc75a14488be5f76f3b48
ljonkergouw <ljonkergouw@deloitte.nl>
2022-11-24 13:42:25 +0100

    Added all relevant files for now

---
+++ PythonTrainingGroupB/main.py
@@ -0,0 +1,6 @@
+print("Welcome to our Investment Game! If you want to sign up, press '0'. If you already have an account, press '1' to log in, if  '2'")
+choice = int(input("Please make your choice: ")
+if choice == 1:
+
+
+
