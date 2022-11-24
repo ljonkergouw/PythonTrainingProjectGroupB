5192395874937304c5397ccda182ac4cec9c8bf9
Louk de Loijer <ldeloijer@deloitte.nl>
2022-11-24 23:05:32 +0100

    Updated portfolio functions

--- PythonTrainingGroupB/Portfolio.py
+++ PythonTrainingGroupB/Portfolio.py
@@ -1,57 +1,68 @@
 import json
 
-with open('Portfolios.json', 'r') as j:
-    portfolios = json.loads(j.read())
+def load_portfolios(path):
+    with open('Portfolios.json', 'r') as j:
+        portfolios = json.loads(j.read())
+    return portfolios
 
 def get_portfolio(user_name):
     if user_name in [portfolio['username'] for portfolio in portfolios['users']]:
-        return [portfolio for portfolio in portfolios['users'] if portfolio['username'] == user_name][0]['portfolio']
+        return [portfolio for portfolio in portfolios['users'] if portfolio['username'] == user_name][0]
     else:
         print('Portfolio does not exist')
-
 def display_portfolio(user_name):
     personal_portfolio = get_portfolio(user_name)
 
     print(f"Portfolio displayed for user {user_name}:")
-    print(f"Your Portfolio contains {len(personal_portfolio['portfolio'])} stocks and your balance is €{personal_portfolio['balance']}!")
+    print(f"Your Portfolio contains {len(personal_portfolio['portfolio'])} stocks and your balance is €{personal_portfolio['portfolio']['balance']}!")
 
-display_portfolio('luuk')
-
-def add_money(user_name, amount):
+def add_money(user_name, portfolios, amount):
     personal_portfolio = get_portfolio(user_name)
-    balance = personal_portfolio['balance']
+
+    balance = get_balance(user_name)
+
     if type(amount) == float or type(amount) == int and amount > 0:
-        balance += amount
-        print(f'Your balance is now {balance}')
+        new_balance = balance + amount
+        change_balance(user_name, portfolios, new_balance)
+        print(f"{amount} is added to your initial balance of {balance} so your new balance is now {personal_portfolio['portfolio']['balance']}")
         return balance
     else:
         print("The amount given is not a positive number!")
 
-def change_balance(user_name, portfolios, amount):
-    personal_portfolio = [portfolio for portfolio in portfolios['users'] if portfolio['username'] == user_name][0]
-
-    user_index = [portfolio['username'] for portfolio in portfolios['users']].index(user_name)
-
-    new_balance = add_money(user_name, amount)
+def change_balance(user_name, portfolios, new_balance):
+    print(portfolios)
+    personal_portfolio = get_portfolio(user_name)
     personal_portfolio['portfolio']['balance'] = new_balance
 
-    portfolios[user_index] = personal_portfolio
-
+    print(portfolios)
     with open('portfolios.json', 'w') as fp:
         json.dump(portfolios, fp)
 
+def get_balance(user_name):
+    return get_portfolio(user_name)['portfolio']['balance']
 
+def withdraw_money(user_name, portfolios, amount):
+    personal_portfolio = get_portfolio(user_name)
 
-change_balance("luuk", portfolios, 5000)
+    balance = get_balance(user_name)
 
-def withdraw_money(self, amount):
-    if amount > self.balance:
+    if amount > balance:
         print("You are trying to withdraw more money than is currently in your balance!")
         print("Please provide an amount which is less or equal to your balance")
 
     elif type(amount) == float or type(amount) == int and amount > 0:
-        print(f"{amount} is deducted from your balance which is {self.balance}")
-        self.balance -= amount
-        print(f'Your balance is now {self.balance}')
+        new_balance = balance - amount
+        change_balance(user_name, portfolios, new_balance)
+        print(f"Your balance is now {personal_portfolio['portfolio']['balance']}")
     else:
         print("The amount given is not a positive number!")
+
+def create_empty_portfolio(portfolios):
+    pass
+
+
+if __name__ == '__main__':
+    portfolios = load_portfolios('Portfolios.json')
+    add_money("luuk", portfolios, 5000)
+    display_portfolio('luuk')
+    withdraw_money('luuk', portfolios, 50000)
