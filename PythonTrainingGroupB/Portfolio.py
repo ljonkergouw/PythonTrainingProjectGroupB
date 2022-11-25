c41484352026ff377ca626d228e70bc237d4d860
Louk de Loijer <ldeloijer@deloitte.nl>
2022-11-25 08:27:11 +0100

    Integrated portfolios.py in main.py

--- PythonTrainingGroupB/Portfolio.py
+++ PythonTrainingGroupB/Portfolio.py
@@ -5,19 +5,20 @@ def load_portfolios(path):
         portfolios = json.loads(j.read())
     return portfolios
 
-def get_portfolio(user_name):
+def get_portfolio(user_name, portfolios):
     if user_name in [portfolio['username'] for portfolio in portfolios['users']]:
         return [portfolio for portfolio in portfolios['users'] if portfolio['username'] == user_name][0]
     else:
         print('Portfolio does not exist')
-def display_portfolio(user_name):
-    personal_portfolio = get_portfolio(user_name)
+def display_portfolio(user_name, portfolios):
+    personal_portfolio = get_portfolio(user_name, portfolios)
 
     print(f"Portfolio displayed for user {user_name}:")
+
     print(f"Your Portfolio contains {len(personal_portfolio['portfolio'])} stocks and your balance is €{personal_portfolio['portfolio']['balance']}!")
 
 def add_money(user_name, portfolios, amount):
-    personal_portfolio = get_portfolio(user_name)
+    personal_portfolio = get_portfolio(user_name, portfolios)
 
     balance = get_balance(user_name)
 
@@ -31,7 +32,7 @@ def add_money(user_name, portfolios, amount):
 
 def change_balance(user_name, portfolios, new_balance):
     print(portfolios)
-    personal_portfolio = get_portfolio(user_name)
+    personal_portfolio = get_portfolio(user_name, portfolios)
     personal_portfolio['portfolio']['balance'] = new_balance
 
     print(portfolios)
@@ -39,10 +40,10 @@ def change_balance(user_name, portfolios, new_balance):
         json.dump(portfolios, fp)
 
 def get_balance(user_name):
-    return get_portfolio(user_name)['portfolio']['balance']
+    return get_portfolio(user_name, portfolios)['portfolio']['balance']
 
 def withdraw_money(user_name, portfolios, amount):
-    personal_portfolio = get_portfolio(user_name)
+    personal_portfolio = get_portfolio(user_name, portfolios)
 
     balance = get_balance(user_name)
 
@@ -57,13 +58,24 @@ def withdraw_money(user_name, portfolios, amount):
     else:
         print("The amount given is not a positive number!")
 
+def check_if_portfolio_exists(portfolios, user_name):
+    if user_name not in [portfolio['username'] for portfolio in portfolios['users']]:
+        choice = input('It seems you do not have a portfolio, want to create one? [yes, no]')
+        if choice == "yes":
+            create_empty_portfolio(portfolios, user_name)
+    else:
+        choice = input(f'Welcome back user {user_name}, want see how your portfolio is doing? [yes, no]')
+        if choice == "yes":
+            display_portfolio(user_name, portfolios)
+
+
 def create_empty_portfolio(portfolios, user_name):
     if user_name not in [portfolio['username'] for portfolio in portfolios['users']]:
         portfolios['users'].append({'username': user_name, 'portfolio':{'balance' : 0, 'stocks' : {}}})
         with open('portfolios.json', 'w') as fp:
             json.dump(portfolios, fp)
         print('Empty portfolio created')
-        display_portfolio(user_name)
+        display_portfolio(user_name, portfolios)
     else:
         print('This username already exists, please create a portfolio with a different username!')
 
--- PythonTrainingGroupB/main.py
+++ PythonTrainingGroupB/main.py
@@ -1,28 +1,43 @@
-# from User import UserName
 # import pandas as pd
 import json
+from Portfolio import *
 
-f = open("accounts.json")
-accounts = json.load(f)
+def load_accounts(path):
+    f = open(path)
+    return json.load(f)
 
-print("Welcome to our Investment Game! If you want to sign up, press '0'. If you already have an account, press '1' to log in.")
+def add_new_account(path, accounts, new_username, new_password):
+    accounts[new_username] = new_password
+    with open(path, 'w') as fp:
+        json.dump(accounts, fp)
 
-choice = int(input("Please make your choice: "))
+def login(accounts):
+    print("Welcome to our Investment Game! If you want to sign up, press '0'. If you already have an account, press '1' to log in.")
 
-if choice == 0:
+    choice = int(input("Please make your choice: "))
+
+    if choice == 0:
+        login = new_login(accounts)
+        return login
+    elif choice == 1:
+        login = existing_login(accounts)
+        return login
+
+def new_login(accounts):
     new_username = input("Please enter a username: ")
     while new_username in accounts:
         print("User name already exists, please enter another username: ")
         new_username = input("Please enter a username: ")
     new_password = input("Please enter a password: ")
 
+    add_new_account('accounts.json', accounts, new_username, new_password)
+
     print("Registration completed")
 
-    accounts[new_username] = new_password
-    with open('accounts.json', 'w') as fp:
-        json.dump(accounts, fp)
+    return new_username
 
-elif choice == 1:
+
+def existing_login(accounts):
     print("Please enter you username.")
 
     while True:
@@ -31,11 +46,18 @@ elif choice == 1:
             existing_password = input("Please enter your password: ")
             if existing_password == accounts[existing_username]:
                 print("Log in succesful")
+                return existing_username
                 break
             else:
                 print("You entered the wrong username/password combination. Please try again.")
         else:
             print("You didn't enter an existing username. Please try again ")
+
+if __name__ == "__main__":
+    accounts = load_accounts("accounts.json")
+    user_name = login(accounts)
+    portfolios = load_portfolios("portfolios.json")
+    check_if_portfolio_exists(portfolios, user_name)
 
 
 
