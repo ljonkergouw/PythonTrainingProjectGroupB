a608e1e25d8a1676b64d8d8c095f84f8cccf9596
Louk de Loijer <ldeloijer@deloitte.nl>
2022-11-25 13:29:40 +0100

    Improved the menu options (again)

--- PythonTrainingGroupB/menu_options.py
+++ PythonTrainingGroupB/menu_options.py
@@ -4,39 +4,69 @@ def investing_choice_menu(user_name, portfolios):
     print(f"Welcome in the main menu user {user_name}! please type any of the options in the terminal to perform the listed actions.")
 
     print("========== Menu ==========")
-    print("=     view portfolio     =")
-    print("=     deposit  money     =")
-    print("=     withdraw money     =")
-    print("=       buy stocks       =")
-    print("=      sell stocks       =")
-    print("=      exit game         =")
+    print("=    1 view portfolio    =")
+    print("=    2 deposit money     =")
+    print("=    3 withdraw money    =")
+    print("=    4 buy stocks        =")
+    print("=    5 sell stocks       =")
+    print("=    6  exit game        =")
     print("==========================")
 
     choice = input('Please type in the menu option:')
 
-    if choice == "view portfolio":
+    if choice == "view portfolio" or int(choice) == 1:
         display_portfolio(user_name, portfolios)
-        print("You are now returning to the main menu")
-        investing_choice_menu(user_name, portfolios)
-    elif choice == "deposit money":
+        return_choice = input("want to return to the menu? [yes, no]")
+        if return_choice == "yes":
+            investing_choice_menu(user_name, portfolios)
+        elif return_choice == "no":
+            print("Exiting Investment Game, Thanks for playing")
+        else:
+            print("not a valid answer, returning to main menu")
+            investing_choice_menu(user_name, portfolios)
+    elif choice == "deposit money" or int(choice) == 2:
         amount = int(input("How much money would you like to deposit to your balance?"))
         add_money(user_name, portfolios, amount)
-        print("You are now returning to the main menu")
-        investing_choice_menu(user_name, portfolios)
-    elif choice == "withdraw money":
+        return_choice = input("want to return to the menu? [yes, no]")
+        if return_choice == "yes":
+            investing_choice_menu(user_name, portfolios)
+        elif return_choice == "no":
+            print("Exiting Investment Game, Thanks for playing")
+        else:
+            print("not a valid answer, returning to main menu")
+            investing_choice_menu(user_name, portfolios)
+    elif choice == "withdraw money" or int(choice) == 3:
         amount = int(input("How much money would you like to withdraw from your balance?"))
         withdraw_money(user_name, portfolios, amount)
-        print("You are now returning to the main menu")
-        investing_choice_menu(user_name, portfolios)
-    elif choice == "buy stocks":
+        return_choice = input("want to return to the menu? [yes, no]")
+        if return_choice == "yes":
+            investing_choice_menu(user_name, portfolios)
+        elif return_choice == "no":
+            print("Exiting Investment Game, Thanks for playing")
+        else:
+            print("not a valid answer, returning to main menu")
+            investing_choice_menu(user_name, portfolios)
+    elif choice == "buy stocks" or int(choice) == 4:
         print("This functionality currently does not exist")
-        print("You are now returning to the main menu")
-        investing_choice_menu(user_name, portfolios)
-    elif choice == "sell stocks":
+        return_choice = input("want to return to the menu? [yes, no]")
+        if return_choice == "yes":
+            investing_choice_menu(user_name, portfolios)
+        elif return_choice == "no":
+            print("Exiting Investment Game, Thanks for playing")
+        else:
+            print("not a valid answer, returning to main menu")
+            investing_choice_menu(user_name, portfolios)
+    elif choice == "sell stocks" or int(choice) == 5:
         print("This functionality currently does not exist")
-        print("You are now returning to the main menu")
-        investing_choice_menu(user_name, portfolios)
-    elif choice == "exit game":
+        return_choice = input("want to return to the menu? [yes, no]")
+        if return_choice == "yes":
+            investing_choice_menu(user_name, portfolios)
+        elif return_choice == "no":
+            print("Exiting Investment Game, Thanks for playing")
+        else:
+            print("not a valid answer, returning to main menu")
+            investing_choice_menu(user_name, portfolios)
+    elif choice == "exit game" or int(choice) == 6:
         print("Exiting Investment Game, Thanks for playing")
     else:
         print("You did not type a valid menu choice")
