afdc7acdd8f8b44a9912ab344f1b2ee35193a91e
Louk de Loijer <ldeloijer@deloitte.nl>
2022-11-28 14:09:27 +0100

    Search implemented

--- PythonTrainingGroupB/menu_options.py
+++ PythonTrainingGroupB/menu_options.py
@@ -1,5 +1,6 @@
 from Portfolio import *
 from transactions import *
+from search import *
 
 def investing_choice_menu(user_name, portfolios):
     print(f"Welcome in the main menu user {user_name}! please type in the number of a menu option in the terminal to perform the action.")
@@ -10,7 +11,8 @@ def investing_choice_menu(user_name, portfolios):
     print("=    3 withdraw money    =")
     print("=    4 buy stocks        =")
     print("=    5 sell stocks       =")
-    print("=    6  exit game        =")
+    print("=    6 search            =")
+    print("=    7 exit game         =")
     print("==========================")
 
     choice = input('Please type in the menu option number: ')
@@ -68,6 +70,16 @@ def investing_choice_menu(user_name, portfolios):
             print("Not a valid answer, returning to main menu.")
             investing_choice_menu(user_name, portfolios)
     elif int(choice) == 6:
+        print('Search Menu')
+        search_input = input("Please enter your search:")
+        results = search(search_input)
+        print_search_results(results)
+        second_search = input("Of which of these stocks would you like to know more information? Please provide the ticker")
+        stock_info(second_search, key)
+        display_price_chart(second_search, key)
+        print('Return to menu')
+        investing_choice_menu(user_name, portfolios)
+    elif int(choice) == 7:
         print("Exiting Investment Game, Thanks for playing.")
     else:
         print("You did not type a valid menu choice.")
